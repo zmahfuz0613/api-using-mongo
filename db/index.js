0b645a4e29c9d7abaf8cfb4d7b5a89947db13888
Bruno Galvao <brunopgalvao@gmail.com>
2020-04-30 18:37:54 -0400

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/productsMongoDatabase'
+
 mongoose
-    .connect('mongodb://127.0.0.1:27017/productsMongoDatabase', { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
         console.log('Successfully connected to MongoDB.');
       })
